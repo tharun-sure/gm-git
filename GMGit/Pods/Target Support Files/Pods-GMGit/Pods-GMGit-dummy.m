f778f49ac693494c17f2e3ff40add0c350ba818f
Tharun Sure <tharunsure@Tharuns-MacBook-Pro.local>
2021-08-09 12:23:41 -0700

    pod init and added support for Alamofire

---
+++ GMGit/Pods/Target Support Files/Pods-GMGit/Pods-GMGit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GMGit : NSObject
+@end
+@implementation PodsDummy_Pods_GMGit
+@end
---
+++ GMGit/Pods/Target Support Files/Pods-GMGitTests/Pods-GMGitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GMGitTests : NSObject
+@end
+@implementation PodsDummy_Pods_GMGitTests
+@end
